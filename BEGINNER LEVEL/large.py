6e4ef025e62b8b5bc12795e93fa1ac4aed864278
RANJITHA-v <37336106+RANJITHA-v@users.noreply.github.com>
2018-03-21 09:31:56 +0530

    Update large.py

--- BEGINNER LEVEL/large.py
+++ BEGINNER LEVEL/large.py
@@ -1,9 +1,9 @@
 a=input("enter the number1")
 b=input("enter the number2")
 c=input("enter the number3")
-if a>b:
+if a>=b and a>=c:
   print ("a")
-elif(b>c):
+elif b>=c:
   print("b ")
 else:
   print("c ")
